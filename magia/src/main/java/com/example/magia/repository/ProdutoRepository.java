efb14c91127796afb75632e1b9033f46450bf529
Kaique Magalhães Santos <kaiquemagalhaes27@gmail.com>
2026-01-26 22:23:20 -0300

    Recomeço da API

--- magia/src/main/java/com/example/magia/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.magia.repository;
-
-import com.example.magia.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, String> {
-}
